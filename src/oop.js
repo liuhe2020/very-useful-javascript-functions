4fa9b68122cd4b2d893574169048d5cd5ad4af39
Liu He <lhe19@yahoo.co.uk>
2024-01-24 21:10:47 +0000

    added 'super' explanation in oop

--- src/oop.js
+++ src/oop.js
@@ -17,7 +17,7 @@ const methods = {
 
 const account1 = accountCreator1('Mormont', 1000);
 const proto1 = Object.getPrototypeOf(account1);
-console.log(account1, proto1);
+// console.log(account1, proto1);
 
 // SOLUTION 2
 function AccountCreator2(name, balance) {
@@ -35,7 +35,7 @@ AccountCreator2.prototype.deposit = function (amount) {
 
 const account2 = new AccountCreator2('Lannister', 2000);
 const proto2 = Object.getPrototypeOf(account2);
-console.log(account2, proto2);
+// console.log(account2, proto2);
 
 // SOLUTION 3
 class AccountCreator3 {
@@ -53,4 +53,22 @@ class AccountCreator3 {
 
 const account3 = new AccountCreator3('Targaryen', 3000);
 const proto3 = Object.getPrototypeOf(account3);
-console.log(account3);
+// console.log(account3, Object.getOwnPropertyNames(proto3));
+
+// EXTRA
+class CompanyAccountCreator extends AccountCreator3 {
+  constructor(name, balance, companyName, loan) {
+    super(name, balance);
+    this.companyName = companyName;
+    this.loan = loan;
+  }
+  getRealBalance() {
+    console.log('balance is');
+    super.getBalance();
+    console.log(`real balance is ${this.balance - this.loan}`);
+  }
+}
+
+const company = new CompanyAccountCreator('Drogo', 4000, 'Drogo Inc', 1000);
+// company.getRealBalance();
+// console.log(Object.getOwnPropertyNames(company.__proto__));
